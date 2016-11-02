1b56725bb0a5d7c7f88ba79b36b4f7df6250e47b
maryah25 <maryah.harris43@gmail.com>
2016-11-02 09:45:39 -0400

    JS-HW RGB

---
+++ class-work/rgb-color-choice/js/project.js
@@ -0,0 +1,17 @@
+document.getElementById('color-button').onclick = changeColor
+
+function changeColor() {
+  var red = document.getElementById('red').value;
+  var green = document.getElementById('green').value;
+  var blue = document.getElementById('blue').value;
+
+  var colorStr = "rgb(" + red + "," + green + "," + blue + ")";
+
+  document.getElementById("colorful-text").innerHTML = colorStr;
+
+  document.getElementById('wrapper').style.background = colorStr;
+}
+
+
+
+
